2a73e308688f943b26ff999af48fd6a2ee187e27
da kong <kongda9999@gmail.com>
2021-09-28 09:45:24 +0800

    tset commit

--- test.js
+++ test.js
@@ -1,49 +1,62 @@
 #! /usr/bin/env node
 const simpleGit = require('simple-git');
 const git = simpleGit();
-const packageJson = require('./package.json');
+const filePath = './package.json';
+const packageJson = require(filePath);
 const fs = require('fs');
 const commander = require('commander');
 const program = new commander.Command();
+const inquirer = require('inquirer');
 
 program
-    .command('major')
+    .command('publish')
+    .description('create a new release')
     .action(() => {
-        test('major');
-    });
-program
-    .command('minor')
-    .action(() => {
-        test('minor');
-    });
-program
-    .command('patch')
-    .action(() => {
-        test('patch');
+        inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'releaseType',
+                    message: 'Choose a release type:',
+                    choices: [
+                        'major',
+                        'minor',
+                        'patch'
+                    ],
+                },
+                {
+                    type: 'input',
+                    name: 'commit',
+                    message: 'Enter commit description:',
+                    default: ''
+                },
+            ])
+            .then((answers) => {
+                test(answers);
+            })
     });
 
 program.parse(process.argv);
 
-async function test(versionType) {
+async function test(answers) {
     try {
         const versionNumbers = packageJson.version.split('.');
         let major = versionNumbers[0];
         let minor = versionNumbers[1];
         let patch = versionNumbers[2];
-        if (versionType === "major") {
+        if (answers.releaseType === "major") {
             major = Number(major) + 1;
         }
-        else if (versionType === "minor") {
+        else if (answers.releaseType === "minor") {
             minor = Number(minor) + 1;
         }
-        else if (versionType === "patch") {
+        else if (answers.releaseType === "patch") {
             patch = Number(patch) + 1;
         }
         const newVersionNumber = `${major}.${minor}.${patch}`;
         packageJson.version = newVersionNumber;
-        fs.writeFile('./package.json', JSON.stringify(packageJson, null, 4), () => { });
-        await git.add('*').commit("test").push().addTag(packageJson.version);
-        //const diffSummary = await git.diff();
+        fs.writeFile(filePath, JSON.stringify(packageJson, null, 4), (err) => { if (err) { console.log(err); } });
+        await git.add('*').commit(answers.commit).push().addTag(packageJson.version);
 
     }
     catch (e) {
