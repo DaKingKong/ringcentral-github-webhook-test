f305d33191e57d3464cc70ae8c8be5f9408119e6
da kong <kongda9999@gmail.com>
2021-09-28 09:16:23 +0800

    test

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 const simpleGit = require('simple-git');
 const git = simpleGit();
+const packageJson = require('./package.json');
+const fs = require('fs');
 
 async function test() {
     try {
-        // await git.add('*').commit("test").push();
-        const diffSummary = await git.diff();
-        console.log(diffSummary);
+        await git.add('*').commit("test").push().addTag("tag1");
+        //const diffSummary = await git.diff();
+        
+        // packageJson.version ="0.0.3";
+        // fs.writeFile('./package.json', JSON.stringify(packageJson, null, 4), ()=>{});
     }
     catch (e) {
         console.log(e);
