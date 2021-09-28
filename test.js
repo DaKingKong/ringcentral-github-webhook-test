bd3215ab64d2899b0ac62c4dfa2006d5bcf6cc21
da kong <kongda9999@gmail.com>
2021-09-28 09:25:52 +0800

    test

--- test.js
+++ test.js
@@ -1,11 +1,37 @@
+#! /usr/bin/env node
 const simpleGit = require('simple-git');
 const git = simpleGit();
 const packageJson = require('./package.json');
 const fs = require('fs');
+const commander = require('commander');
+const program = new commander.Command();
 
-async function test() {
+program
+    .command('publishMajor')
+    .action(()=>{
+
+    });
+
+async function test(versionType) {
     try {
-        packageJson.version ="0.0.4";
+        const versionNumbers = packageJson.version.split('.');
+        let major = versionNumbers[0];
+        let minor = versionNumbers[1];
+        let patch = versionNumbers[2];
+        if(versionType === "major")
+        {
+            major = Number(major)++;
+        }
+        else if(versionType === "minor")
+        {
+            minor = Number(minor)++;
+        }
+        else if(versionType === "patch")
+        {
+            patch = Number(patch)++;
+        }
+        const newVersionNumber = `${major}.${minor}.${patch}`;
+        packageJson.version = newVersionNumber;
         fs.writeFile('./package.json', JSON.stringify(packageJson, null, 4), ()=>{});
         await git.add('*').commit("test").push().addTag(packageJson.version);
         //const diffSummary = await git.diff();
